282ae4f5c40ba1c6837e16baa2000d63ceed1295
tumi2010 <122444376+tumi2010@users.noreply.github.com>
2023-01-11 11:25:27 +0100

    Actualizar pxt.json, custom.ts, tommy.ts, maimona.ts, main.py

---
+++ custom.ts
@@ -0,0 +1,45 @@
+
+/**
+* Utilice este archivo para definir funciones y bloques personalizados.
+* Lea más en https://makecode.microbit.org/blocks/custom
+*/
+
+enum MyEnum {
+    //% block="one"
+    One,
+    //% block="two"
+    Two
+}
+
+/**
+ * Custom blocks
+ */
+//% weight=100 color=#0fbc11 icon=""
+namespace custom {
+    /**
+     * TODO: describe your function here
+     * @param n describe parameter here, eg: 5
+     * @param s describe parameter here, eg: "Hello"
+     * @param e describe parameter here
+     */
+    //% block
+    export function foo(n: number, s: string, e: MyEnum): void {
+        // Add code here
+    }
+
+    /**
+     * TODO: describe your function here
+     * @param value describe value here, eg: 5
+     */
+    //% block
+    export function fib(value: number): number {
+        return value <= 1 ? value : fib(value -1) + fib(value - 2);
+    }
+}
+music.playMelody("", 120)
+music.stopAllSounds()
+if (true) {
+    music.playTone(Note.C, music.beat(BeatFraction.Whole))
+    
+    custom.foo(5, "Hello", MyEnum.One)
+}
---
+++ tommy.ts
@@ -0,0 +1,1 @@
+// Añade tu código aquí
